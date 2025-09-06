d6b079aab68f35ff0afd0171addad023308dba8a
bartosz121 <bmagiera121@gmail.com>
2025-09-06 16:40:52 +0200

    Add fts to esp-ebi endpoint
      - fix endpoint path

--- gpw_scraper/models/espi_ebi.py
+++ gpw_scraper/models/espi_ebi.py
@@ -1,7 +1,9 @@
 import enum
 from datetime import datetime
+from typing import Any
 
-from sqlalchemy import TIMESTAMP
+from sqlalchemy import TIMESTAMP, Computed, Index
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column
 
 from gpw_scraper import utils
@@ -15,6 +17,13 @@ class EntryType(enum.StrEnum):
 
 class EspiEbi(BaseModel):
     __tablename__ = "espi_ebi"
+    __table_args__ = (
+        Index(
+            "ix_espi_ebi_tsvector",
+            "_tsvector",
+            postgresql_using="gin",
+        ),
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     type: Mapped[EntryType] = mapped_column()
@@ -25,3 +34,17 @@ class EspiEbi(BaseModel):
     parsed_by_llm: Mapped[str | None] = mapped_column()
     date: Mapped[datetime] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=utils.utc_now)
+
+    _tsvector: Mapped[Any] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+setweight(to_tsvector('pl_ispell', title), 'A') ||
+setweight(to_tsvector('pl_ispell', description), 'B') ||
+setweight(to_tsvector('pl_ispell', company), 'D')
+""",
+            persisted=True,
+        ),
+        deferred=True,
+        deferred_raiseload=True,
+    )
